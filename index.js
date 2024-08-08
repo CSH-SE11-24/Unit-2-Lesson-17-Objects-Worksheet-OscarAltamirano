31160bffbeb92bee918de5428cd36eb88b85f1b5
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 11:39:11 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,51 @@
+// Task 1
+// Create a character object with 3 key:value pairs (e.g. name, show, age)
+let character = {
+  name: "Barry Allen",
+  show: "The Flash",
+  age: 33
+}
+
+
+
+// Task 2
+// Console log the name of the character
+console.log(character.name)
+
+// Task 3
+// Modify one of the keys to be a different value
+character.name = "Joe West"
+
+
+// Task 4
+// Add a new key value pair to the object that is an array (e.g. friends, catchphrases)
+character.friends = ["Iris ", "Nora", "Cisco "]
+
+// Task 5 (Stretch)
+// Make an array called characters that has at least 3 character objects in it
+// You can make the objects directly in the array or push the objects to the array
+let characters = [
+  {
+  name: "Barry Allen",
+  show: "The flash",
+  age: 33,
+ },
+ { 
+  name: "Beast boy",
+  show: "Titans",
+  age: 28,
+ },
+ {
+  name: "Oliver queen",
+  show: "Arrow",
+  age: 42,
+},
+]
+
+// Console log the first character
+console.log(characters[0])
+
+// Console log the name of the first character
+console.log(characters[0].name)
+
+
